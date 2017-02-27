852d3cc513986a0d86182f011c638e1b229044f7
Pavel Egorov <snake7799@users.noreply.github.com>
2017-02-27 22:37:02 +0300

    product-of-2-numbers/feature: implement multiply method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    let result = [];
+
+    first = first.split('').reverse();
+    second = second.split('').reverse();
+
+    for (let i = 0; i < first.length; i++) {
+        for (let j = 0; j < second.length; j++) {
+            let multiply = first[i] * second[j];
+            result[i + j] = (result[i + j] != undefined) ? result[i + j] + multiply : multiply;
+        }
+    }
+
+    for (let i = 0; i < result.length; i++) {
+        let number = result[i] % 10;
+        let carry = Math.floor(result[i] / 10);
+        result[i] = number;
+
+        if (result[i + 1] != undefined)
+            result[i + 1] += carry;
+        else if (carry != 0)
+            result[i + 1] = carry;
+    }
+
+    return result.reverse().join('').replace(/^(0(?!$))+/, "");
 }
